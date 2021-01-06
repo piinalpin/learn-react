c4797c369460a65c039f0af1fb710151b651b1b6
Alvinditya Saputra <alvindityas@gmail.com>
2021-01-06 14:56:36 +0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,91 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { graphql } from 'react-apollo';
+import gql from 'graphql-tag';
+import { Container, Feed, Card } from 'semantic-ui-react';
+import 'semantic-ui-css/semantic.min.css'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    console.log(this.props.data);
+    return (
+        <Container text textAlign="center">
+          <Card centered fluid>
+            <Card.Content>
+              <Card.Header>Person Data</Card.Header>
+            </Card.Content>
+            <Card.Content>
+              {this.props.data.getAllPerson && (
+                <Feed>
+                  {this.props.data.getAllPerson.map(person => (
+                      <Feed.Event key={person.id}>
+                        <Feed.Content>
+                          <Feed.Label content={person.firstName} />
+                          <Feed.Date content={person.createdAt} />
+                          <Feed.Summary>{person.address}</Feed.Summary>
+                        </Feed.Content>
+                      </Feed.Event>
+                    ))}
+                  </Feed>
+                )
+              }
+            </Card.Content>
+          </Card>
+          <Card centered fluid>
+            <Card.Content>
+              <Card.Header>Book Data</Card.Header>
+            </Card.Content>
+            <Card.Content>
+              {this.props.data.getAllPerson && (
+                <Feed>
+                  {this.props.data.getAllBook.map(book => (
+                      <Feed.Event key={book.id}>
+                        <Feed.Content>
+                          <Feed.Label>{book.title} - {book.author.firstName}</Feed.Label>
+                          <Feed.Date content={book.releaseDate} />
+                          <Feed.Summary>{book.description}</Feed.Summary>
+                        </Feed.Content>
+                      </Feed.Event>
+                    ))}
+                  </Feed>
+                )
+              }
+            </Card.Content>
+          </Card>
+        </Container>
+    );
+  }
 }
 
-export default App;
+const queries = gql`
+  query {
+    getAllPerson {
+      id
+      firstName
+      lastName
+      address
+      createdAt
+    }
+    getAllBook {
+        id
+        title
+        releaseDate
+        description
+        author {
+            id
+            firstName
+            address
+            createdAt
+        }
+        createdAt
+    }
+  }
+`;
+
+export default graphql(queries, {
+  options: {
+    variables: {
+
+    }
+  }
+})(App);
--- src/index.js
+++ src/index.js
@@ -1,14 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ApolloClient } from 'apollo-client';
+import { HttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { ApolloProvider } from 'react-apollo';
+
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const client = new ApolloClient({
+	link: new HttpLink({
+		uri: 'http://localhost:8080/graphql'
+	}),
+	cache: new InMemoryCache()
+});
+
 ReactDOM.render(
-  <React.StrictMode>
+  <ApolloProvider client={client}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </ApolloProvider>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
